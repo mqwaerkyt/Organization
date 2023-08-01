48dc35e91b8a92b651549f8d74a287ad2df5f568
manme <manme@DESKTOP-8QORG7H>
2023-08-01 02:41:28 -0400

    Add project files.

---
+++ Organization/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Organization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+
+
