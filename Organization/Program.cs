488b73662744cc10ba3d5349d86183535c555aac
mqwaerkyt <manmeerg@gmail.com>
2023-08-31 12:19:16 -0400

    Update Program.cs

--- Organization/Program.cs
+++ Organization/Program.cs
@@ -5,9 +5,22 @@ namespace Organization
 {
     internal class Program
     {
+
+        public int speed;
+        public string color;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int time = 10;
+            string result = (time < 18) ? "Good day." : "Good evening.";
+            Console.WriteLine(result);
+
+            Program MyObj = new Program();
+
+            MyObj.speed = 1;
+
+            MyObj.color = "red";
         }
     }
 }
