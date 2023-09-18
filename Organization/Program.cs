fca2f17fe3eb17cbeb2311ab651f300a2fee89d4
mqwaerkyt <manmeerg@gmail.com>
2023-09-18 12:50:27 -0400

    Update Program.cs

--- Organization/Program.cs
+++ Organization/Program.cs
@@ -6,80 +6,104 @@ namespace Organization
     internal class Program
     {
 
-        public int speed;
-        public string color;
+        //public int speed;
+        //public string color;
 
-        public List<int> lol = new List<int>(); 
+        public static List<int> lol = new List<int>();
+        public static List<char> lol_char = new List<char>();
+        public static List<int> lol_int = new List<int>();
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            int time = 9;
-            string result = (time < 18) ? "Good day." : "Good evening.";
-            Console.WriteLine(result);
-
-            Program MyObj = new Program();
-
-            MyObj.speed = 1;
-
-            MyObj.color = "red";
-            
-            string x = Console.ReadLine();
-
-            x = x.ToString();
-
-            char[] x_array= x.ToArray();
-
-            Console.WriteLine(x_array.Length);
-            Console.WriteLine(x_array[1]);
-
-            string bazString = @"Here's some stuff
-on a new line! ""Wow!"", the masses cried";
-
-            Console.WriteLine(bazString);
-
-            Console.WriteLine(2+1/2);
+    //            Console.WriteLine("Hello World!");
+    //            int time = 9;
+    //            string result = (time < 18) ? "Good day." : "Good evening.";
+    //            Console.WriteLine(result);
+    //            //Program MyObj = new Program();
+    //            //MyObj.speed = 1;
+    //            MyObj.color = "red";
+    //            string x = Console.ReadLine();
+    //            x = x.ToString();
+    //            char[] x_array= x.ToArray();
+    //            Console.WriteLine(x_array.Length);
+    //            Console.WriteLine(x_array[1]);
+    //            string bazString = @"Here's some stuff
+    //on a new line! ""Wow!"", the masses cried";
+    //            Console.WriteLine(bazString);
+    //            Console.WriteLine(2+1/2);
 
 
 
             Rate_Law();
         }
-        public static void Rate_Law() 
+        public static void Rate_Law()
         {
-            Console.WriteLine("h");
+            Console.WriteLine("list moment");
             string a = Console.ReadLine();
-            string b = Console.ReadLine();
-            string b = Console.ReadLine();
+            //Console.WriteLine("repeated");
+            //string b = Console.ReadLine();
+            //Console.WriteLine("repeated");
+            //string c = Console.ReadLine();
 
             foreach (char i in a)
             {
-                
-                if (i-'0' >= 0) 
+
+                if (i - '0' >= 0)
                 {
                     lol.Add(i - '0');
                 }
             }
 
-            foreach (char i in b)
+            int lol_amount = lol.Count;
+            int ppp = 0;
+            Console.WriteLine(lol_amount);
+            int uppercase = 32;
+
+            while(ppp < lol_amount)
             {
-
-                if (i - '0' >= 0)
+                if (lol[ppp] < 10)
                 {
-                    Console.WriteLine(i);
+                    Console.WriteLine(lol[ppp]);
+                    lol_int.Add(lol[ppp]);
                 }
-            }
-
-            foreach (char i in c)
-            {
-
-                if (i - '0' >= 0)
+                else 
                 {
-                    Console.WriteLine(i);
+                    Console.WriteLine("h moment : " + (char)(lol[ppp]+16+(uppercase)));
+                    lol_char.Add((char)(lol[ppp] + 16 + (uppercase)));
                 }
-            }
 
-            float f = -1/(2) ;
+                
+            ppp++;
+
+            }
+         
+
+            lol_int.ForEach(p => Console.Write(p+","));
+            lol_char.ForEach(p => Console.Write(p + ","));
+
+            //foreach (char i in b)
+            //{
+
+            //    if ((i - '0') >= (0) )
+            //    {
+            //        Console.WriteLine("your ints were 0:"+ i);
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("these aren't ints :" + i);
+            //    }
+            //}
+
+            //foreach (char i in c)
+            //{
+
+            //    if (i - '0' >= 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
 
 
         }
